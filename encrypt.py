ff9f91cc33f19aa7dfb108d0e8593d819f4c2504
Jason Ng <jng40@stuy.edu>
2024-05-23 14:50:36 -0400

    started coding the chaocipher

---
+++ encrypt.py
@@ -0,0 +1,13 @@
+#Chaocipher
+
+def encrypt(text, key1, key2):
+    # text 
+    text = text.lower()
+    # 
+
+
+if __name__ == "__main__":
+    text = "Hello, World!"
+    key1 = "HXUCZVAMDSLKPEFJRIGTWOBNYQ"
+    key2 = "PTLNBQDEOYSFAVZKGJRIHWXUMC"
+    print(encrypt(text, key1, key2))
