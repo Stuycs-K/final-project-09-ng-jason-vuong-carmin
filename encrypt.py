c7a1b6aa319fc239e10c0ea1002a90dfff1ba89b
Jason Ng <jng40@stuy.edu>
2024-05-24 14:50:26 -0400

    final commit

--- encrypt.py
+++ encrypt.py
@@ -15,6 +15,11 @@ def encrypt(text, cipherText, plainText):
                 break
     return output
 
+def shift(text, cipherText, plainText)[
+    temp = cipherText[0] 
+    
+]
+
 def swap (key2, index):
     temp = key2[index]
     key2[index] = key2[(index + 13) % __KEYSIZE__]
@@ -25,17 +30,18 @@ def reorder (cipherText, index):
     # reorder the ciphertext 
     # this is t
     temp = cipherText[(index + 1) % __KEYSIZE__]
-    for i in range(0, __KEYSIZE__):
+    for i in range(0, __NADIR__):
+        swap(cipherText, (index + i) % __KEYSIZE__)
         #pseudo code 
         #swap the ith element with the ith + 13 element
         #then move everything down  
+        
 
-    key1 = []
     key2 = []
     for i in range(0, 26):
         key1.append(plainText[cipherText.index(chr(65 + i))])
         key2.append(cipherText[i])
-    return key1, key2
+    return 
 
 
 if __name__ == "__main__":
