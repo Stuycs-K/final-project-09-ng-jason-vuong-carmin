651f0e090a7f43dd154309509dfc12636eacf928
Jason Ng <jng40@stuy.edu>
2024-05-28 16:59:09 -0400

    finished decrypt

--- decrypt.py
+++ decrypt.py
@@ -3,10 +3,18 @@
 import encrypt
 
 def decrypt(text, cipherText, plainText):
-    return encrypt.encrypt(text, plainText, cipherText)
+    text = text.upper()
+    output = ""
+    for char in text: 
+        plainText, cipherText = encrypt.shift(char, plainText, cipherText)
+        #print(cipherText)
+        output += plainText[0]
+        cipherText = encrypt.fixedCipher(cipherText) 
+        plainText = encrypt.fixedPlain(plainText)
+    return output
 
 if __name__ == "__main__":
     text = "GZNDZ"
     cipherText = list("CHAOBDEFGIJKLMNPQRSTUVWXYZ")
-    plainText = list("CIPHERABDFGJKLMNOQSTUVWXYZ")
+    plainText =  list("CIPHERABDFGJKLMNOQSTUVWXYZ")
     print(decrypt(text, cipherText, plainText))
