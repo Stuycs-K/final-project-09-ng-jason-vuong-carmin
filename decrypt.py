8ab821ee3d4dfb83b31a52d4f2d9783100f1e24d
Jason Ng <jng40@stuy.edu>
2024-05-28 16:48:13 -0400

    decrypt still doesn't work

--- decrypt.py
+++ decrypt.py
@@ -2,19 +2,11 @@
 
 import encrypt
 
-def decrypt(text, key1, key2):
-    output = ""
-    text = text.upper()
-    for char in text:
-        for i in range(0, 26): 
-            if char == key2[encrypt.__KEYSIZE__ - i - 1]: 
-                output += key1[i]
-                key2 = encrypt.swap(key2, encrypt.__KEYSIZE__ - i - 1)
-                break
-    return output
+def decrypt(text, cipherText, plainText):
+    return encrypt.encrypt(text, plainText, cipherText)
 
 if __name__ == "__main__":
-    text = "CCKLBQJYKJ"
-    key1 = list("HXUCZVAMDSLKPEFJRIGTWOBNYQ")
-    key2 = list("PTLNBQDEOYSFAVZKGJRIHWXUMC")
-    print(decrypt(text, key1, key2))
+    text = "GZNDZ"
+    cipherText = list("CHAOBDEFGIJKLMNPQRSTUVWXYZ")
+    plainText = list("CIPHERABDFGJKLMNOQSTUVWXYZ")
+    print(decrypt(text, cipherText, plainText))
